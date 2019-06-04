cfbf000045f36609f9ad2960fe4fcb9da0dc70e4
dphong <dphong@kaoni.com>
2019-06-04 09:12:58 +0900

    * 1463 Prob commit

--- src/hong/Hong1463.java
+++ src/hong/Hong1463.java
@@ -5,6 +5,30 @@ public class Hong1463 {
 
     public Hong1463() {}
     public void solution() {
+		//오답///////////////////////
+		import java.util.Scanner;
+
+		public class Main {
+			public static void main(String[] args) {
+				Scanner sc = new Scanner(System.in);
+				int x = sc.nextInt();
+				int cnt = 0;
+				while(x != 1) {
+					cnt++;           
+					if(x % 3 == 0) {
+						x = x / 3;
+					} else if ((x - 1) % 3 == 0) {
+						x--;
+					} else if (x % 2 == 0) {
+						x = x / 2;
+					} else {
+						x--;
+					}
+				}
+				sc.close();
+				System.out.println(cnt);
+			}
+		}
     }
 }
 
