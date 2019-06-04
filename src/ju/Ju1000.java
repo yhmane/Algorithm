1764b01f266dda2659c271fb8a2894c082183bbb
Juhongsun <juhongsun@kaoni.com>
2019-06-04 17:18:30 +0900

    테스트4

---
+++ src/ju/Ju1000.java
@@ -0,0 +1,18 @@
+package ju;
+
+import java.util.Scanner;
+
+
+public class Ju1000 {
+
+    public Ju1000() {}
+    public void solution() {
+    	Scanner sc = new Scanner(System.in);
+    	int a = sc.nextInt();
+    	int b = sc.nextInt();
+    	System.out.println(a + b);
+    }
+}
+
+
+
