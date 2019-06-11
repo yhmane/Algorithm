a29f00200723cac07c9851175086492f74c5eea5
Juhongsun <juhongsun@kaoni.com>
2019-06-11 10:30:45 +0900

    2751 풀이

--- src/ju/Ju1463.java
+++ src/ju/Ju1463.java
@@ -1,10 +1,43 @@
 package ju;
 
+import java.util.Scanner;
+
 
 public class Ju1463 {
 
     public Ju1463() {}
-    public void solution() {
+    
+    public static int a[];
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int c = sc.nextInt();
+    	a = new int[c+1];
+    	
+    	System.out.println(solution(c));
+    	sc.close();
+	}
+    
+    public static int solution(int b) {
+    	if(b == 1) {
+    		return 0;
+    	}
+    	if(a[b] > 0) {
+    		return a[b];
+    	}
+    	a[b] = solution(b-1) + 1;
+    	if(b%2 == 0) {
+    		int temp = solution(b/2) + 1;
+    		if(a[b] > temp) {
+    			a[b] = temp;
+    		}
+    	}
+    	if(b%3 == 0) {
+    		int temp = solution(b/3) + 1;
+    		if(a[b] > temp) {
+    			a[b] = temp;
+    		}
+    	}
+    	return a[b];
     }
 }
 
