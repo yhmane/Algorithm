1ffd16869123b2b558a177873dcea1e667db9309
dphong <dphong@kaoni.com>
2019-06-12 10:43:05 +0900

    prob. 10952, 11021, 11022, 11718, 11719

---
+++ src/hong/Hong11022.java
@@ -0,0 +1,18 @@
+package hong;
+
+import java.util.Scanner;
+
+public class Hong11022 {
+	public Hong11022() {}
+	public void solution() {
+		Scanner sc = new Scanner(System.in);
+		int caseNum = sc.nextInt();
+		
+		for(int i = 1; i <= caseNum; i++) {
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			System.out.println("Case #" + i + ": " + n1 + " + " + n2 +  " = " +  (n1 + n2));
+		}
+		sc.close();
+	}
+}
