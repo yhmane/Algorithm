c2be02ca72baa3da418f188c4d72896b96e595d0
mslim8803 <mslim8803@kaoni.com>
2019-06-17 10:21:58 +0900

    임민석

--- src/lim/Lim11057.java
+++ src/lim/Lim11057.java
@@ -4,31 +4,30 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-// TODO 효율성 문제 해결 필요
-public class Lim10844 {
+//TODO 효율성 문제 해결 필요
+public class Lim11057 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		long[] cnt = {0, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+		long[] cnt = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 		
 		for(int i = 0; i < n - 1; i++) {
 			long[] tmp = new long[10];
-			tmp[1] += cnt[0];
 			
-			for(int j = 1; j < 9; j++) {
-				tmp[j - 1]+= cnt[j];
-				tmp[j + 1]+= cnt[j];
+			for(int j = 0; j < 10; j++) {
+				for(int k = j; k < 10; k++) {
+					tmp[k] += cnt[j];
+				}
 			}
-			
-			tmp[8]+= cnt[9];
-			
+						
 			cnt = tmp;
 			
 			for(int k = 0; k < 10; k++) {
-				cnt[k] %= 1_000_000_000;
+				cnt[k] %= 10007;
 			}
 		}
 		
-		System.out.println(Arrays.stream(cnt).sum() % 1_000_000_000);
+		System.out.println(Arrays.stream(cnt).sum() % 10007);
+		
 	}
 }
