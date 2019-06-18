49f2af98c234f46a8e68f8083960f711181a7f4f
mslim <mslim8803@naver.com>
2019-06-18 10:16:54 +0900

    Lim

---
+++ src/lim/_Lim1654.java
@@ -0,0 +1,19 @@
+package lim;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _Lim1654 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int k = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i < k; i++) {
+			int l = Integer.parseInt(br.readLine());
+		}
+	}
+
+}
