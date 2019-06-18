eaeba58775e3a06a1ffe06f80f37bb2b7d9a9a49
mslim8803 <mslim8803@kaoni.com>
2019-06-18 19:56:20 +0900

    LIM

--- src/lim/_Lim1654.java
+++ src/lim/_Lim1654.java
@@ -2,6 +2,9 @@ package lim;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class _Lim1654 {
@@ -11,9 +14,57 @@ public class _Lim1654 {
 		int k = Integer.parseInt(st.nextToken());
 		int n = Integer.parseInt(st.nextToken());
 		
+		@SuppressWarnings("unchecked")
+		Queue<Integer>[] elems = new LinkedList[k];
+		
 		for(int i = 0; i < k; i++) {
-			int l = Integer.parseInt(br.readLine());
+			int number = Integer.parseInt(br.readLine());			
+			elems[i] = new LinkedList<Integer>();
+			
+			for(int j = 1; j <= n; j++) {				
+				elems[i].add(number / j);
+			}		
 		}
+		
+		int[] b = new int[k];
+		int maxIdx = 0;
+		int c = k;
+		int[] d = new int[k];
+		
+		for(int i = 0; i < k; i++) {
+			b[i] = elems[i].poll();
+			d[i] = elems[i].peek();
+			
+			if(d[i] > d[maxIdx]) {
+				maxIdx = i;
+			}
+		}
+		
+		while(true) {
+			if(c == n) {
+				break;
+			}
+			
+			b[maxIdx] = elems[maxIdx].poll();
+			d[maxIdx] = elems[maxIdx].peek();
+			c++;
+			
+			for(int i = 0; i < k; i++) {				
+				if(d[i] > d[maxIdx]) {
+					maxIdx = i;
+				}
+			}
+		}
+		
+		int min = b[0];
+		
+		for(int i = 1; i < b.length; i++) {
+			
+			if(b[i] < min) {
+				min = b[i];
+			}
+		}
+		
+		System.out.println(Arrays.toString(b));	
 	}
-
 }
