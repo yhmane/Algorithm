ed3e1d66d6111dfc0840c40da4945cbd8a1bdb54
mslim8803 <mslim8803@kaoni.com>
2019-06-24 16:59:17 +0900

    asdf

---
+++ src/lim/_Lim1107.java
@@ -0,0 +1,17 @@
+package lim;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _Lim1107 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		
+		if(m > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+		}
+	}
+}
