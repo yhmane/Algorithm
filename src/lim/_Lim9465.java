54000a6c1ed37dec8b1054c36aa4d86264c5948b
mslim <mslim8803@naver.com>
2019-07-01 23:30:12 +0900

    Lim

---
+++ src/lim/_Lim9465.java
@@ -0,0 +1,15 @@
+package lim;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _Lim9465 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < t; i++) {
+			int n = Integer.parseInt(br.readLine());
+		}
+	}
+}
