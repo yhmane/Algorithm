0f39b5512b2a2cb5b536a79b53cb3b053104865d
mslim8803 <mslim8803@kaoni.com>
2019-07-02 18:55:30 +0900

    Lim

---
+++ src/lim/_Lim2156.java
@@ -0,0 +1,44 @@
+package lim;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// max 배열을 이용해서도 해봅시다
+public class _Lim2156 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] wine = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			wine[i] = Integer.parseInt(br.readLine());
+		}
+		
+		if(n == 1) {
+			System.out.println(wine[0]);
+			return;
+		} else if(n == 2) {
+			System.out.println(wine[0] + wine[1]);
+			return;
+		}
+		
+		int a = wine[1] + wine[2];
+		int b = wine[0] + wine[2];
+		int c = wine[0] + wine[1];
+		int d = wine[0];
+				
+		for(int i = 3; i < n; i++) {	
+			int tmpA = b + wine[i];
+			int tmpB = Math.max(c + wine[i], d + wine[i]);
+			int tmpC = Math.max(a, b);
+			int tmpD = Math.max(c, d);
+			
+			a = tmpA;
+			b = tmpB;
+			c = tmpC;	
+			d = tmpD;
+		}
+		
+		System.out.println(Math.max(Math.max(a, b), Math.max(c, d)));
+	}
+}
--- src/lim/_Lim9465.java
+++ src/lim/_Lim9465.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Lim9465 {
+public class _Lim9465 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
