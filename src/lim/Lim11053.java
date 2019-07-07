132945c50996cdd3930616bd76ab95be308b939f
mslim <mslim8803@naver.com>
2019-07-07 21:38:33 +0900

    Lim

---
+++ src/lim/Lim11053.java
@@ -0,0 +1,44 @@
+package lim;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Lim11053 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] seq = new int[n];
+		int[] max = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		max[0] = 1;
+		
+		for(int i = 1; i < n; i++) {
+			max[i] = 1;
+			
+			for(int j = i - 1; j >= 0; j--) {
+				
+				if(seq[j] < seq[i] && max[j] + 1 >  max[i]) {
+					max[i] = max[j] + 1;
+				}
+			}
+		}
+		
+		int answer = max[n - 1];
+		
+		for(int i = n - 2; i >= 0; i--) {
+			
+			if(max[i] > answer) {
+				answer = max[i];
+			}
+		}
+
+		System.out.println(answer);
+	}
+}
--- src/lim/Lim11055.java
+++ src/lim/Lim11055.java
@@ -4,11 +4,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class _Lim11053 {
+public class Lim11055 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int[] seq = new int[n];
+		int[] max = new int[n];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
@@ -16,32 +17,28 @@ public class _Lim11053 {
 			seq[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int compV_b = 0;
-		int compV_c = seq[0];
-		int compV_s = 0;
-		int max1 = 1;
-		int max2 = 0;
+		max[0] = seq[0];
 		
 		for(int i = 1; i < n; i++) {
+			max[i] = seq[i];
 			
-			if(compV_c < seq[i]) {
-				max1++;
-				compV_b = compV_c;
-				compV_c = seq[i];
-			} else if(compV_b < seq[i]) {
-				compV_c = seq[i];
-			} else if(compV_s < seq[i]) {
-				max2++;
-				compV_s = seq[i];
-			}
-			
-			if(max1 == max2) {
-				compV_c = compV_s;
-				compV_s = 0;
-				max2 = 0;
+			for(int j = i - 1; j >= 0; j--) {
+				
+				if(seq[j] < seq[i] && max[j] + seq[i] >  max[i]) {
+					max[i] = max[j] + seq[i];
+				}
 			}
 		}
 		
-		System.out.println(max1);
+		int answer = max[n - 1];
+		
+		for(int i = n - 2; i >= 0; i--) {
+			
+			if(max[i] > answer) {
+				answer = max[i];
+			}
+		}
+
+		System.out.println(answer);
 	}
 }
