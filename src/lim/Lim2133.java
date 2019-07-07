a30f1fc5f074a847d937f11f945d825bf98613d5
mslim <mslim8803@naver.com>
2019-07-07 20:29:08 +0900

    Lim

--- src/lim/Lim2133.java
+++ src/lim/Lim2133.java
@@ -3,7 +3,7 @@ package lim;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class _Lim2133 {
+public class Lim2133 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
@@ -13,10 +13,21 @@ public class _Lim2133 {
 			return;
 		}
 		
-		int[] cnt = new int[n + 1];
-		cnt[2] = 3;
+		n /= 2;
 		
-		for(int i = 4; i <= n; i += 2) {
+		long[] dp = new long[n + 1];
+		
+		dp[0] = 1;
+		dp[1] = 3;
+		
+		for(int i = 2; i <= n; i++) {		
+			for(int j = 0; j < i - 1; j++) {
+				dp[i] += 2 * dp[j];
+			}
+			
+			dp[i] += 3 * dp[i - 1];
 		}
+		
+		System.out.println(dp[n]);
 	}
 }
