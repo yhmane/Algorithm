3e0f11ccb8413bf65c459708783188e365380000
mslim <mslim8803@naver.com>
2019-07-08 23:31:31 +0900

    Lim

--- src/lim/_Lim1912.java
+++ src/lim/_Lim1912.java
@@ -2,11 +2,43 @@ package lim;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+// need to be refactored in terms of efficiency
 public class _Lim1912 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] seq = new int[n];		
+		int[] seq = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] sum = new int[n];		
+		sum[0] = seq[0];
+		
+		for(int i = 1; i < n; i++) {
+			sum[i] = seq[i] + sum[i - 1];
+		}
+		
+		int max = sum[0];
+		
+		for(int i = 1; i < n; i++) {
+			
+			if(max < sum[i]) {
+				max = sum[i];
+			}
+						
+			for(int j = 0; j < i; j++) {
+				
+				if(max < sum[i] - sum[j]) {
+					max =  sum[i] - sum[j];
+				}
+			}
+		}
+		
+		System.out.println(max);
 	}
 }
