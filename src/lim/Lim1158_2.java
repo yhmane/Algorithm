a46622dbd8f592774455f7447475c43769090528
mslim8803 <mslim8803@kaoni.com>
2019-07-26 16:11:42 +0900

    Lim

--- src/lim/Lim1158_2.java
+++ src/lim/Lim1158_2.java
@@ -5,35 +5,41 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
-// 시간 최적화 필요
-public class _Lim1158_2 {
+public class Lim1158_2 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		
-		StringBuffer sb = new StringBuffer();
-		sb.append("<");
+		StringBuffer sb = new StringBuffer("<");
 		
 		List<Integer> list = new LinkedList<>();
-		IntStream.rangeClosed(1, n).forEach(list::add);
+		
+		for(int i = 1; i <= n; i++) {
+			list.add(i);
+		}
+		
 		int i = k - 1;
 		
 		while(true) {
-			sb.append(list.remove(i) + ", ");	
+			sb.append(list.remove(i));	
 			
-			if(list.isEmpty()) break;
+			if(list.isEmpty()) {
+				sb.append(">");
+				break;
+			} else {
+				sb.append(", ");
+			}
 			
 			i += k - 1;
-			while(i > list.size() - 1) {
-				i -= list.size();
+			
+			if (i > list.size() - 1) {
+				i %= list.size();
 			}
 		}
-		
-		sb.delete(sb.length() - 2, sb.length());
-		System.out.println(sb.append(">"));
+
+		System.out.println(sb);
 	}
 }
