f2a666b7fba589c897268ee12d48c9fcf480a417
mslim <mslim8803@naver.com>
2019-08-07 21:52:56 +0900

    Lim

--- src/lim/Lim2004.java
+++ src/lim/Lim2004.java
@@ -5,50 +5,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class _Lim2004 {
-	private static int two = 0;
-	private static int five = 0;
-	
+public class Lim2004 {	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		
-		for(int i = 0; i < m; i++) {
-			cal(n--);
-		}
-		
-		for(int i = 2; i <= m; i++) {
-			cal2(i);
-		}
+		int two = get2(n) - get2(n - m) - get2(m);
+		int five = get5(n) - get5(n - m) - get5(m);
 		
 		System.out.println(Math.min(two, five));
 	}
 	
-	private static void cal(int n) {
+	// n 팩토리얼에 있는 2의 갯수를 구하는 함수
+	private static int get2(int n) {
+		int tmp = n;
+		int cnt = 0;
 		
-		while(n % 2 == 0) {
-			n /= 2;
-			two++;
+		while(tmp > 0) {
+			cnt += tmp/2;
+			tmp /= 2;
 		}
 		
-		while(n % 5 == 0) {
-			n /= 5;
-			five++;
-		}
+		return cnt;
 	}
 	
-	private static void cal2(int n) {
+	// n 팩토리얼에 있는 5의 갯수를 구하는 함수
+	private static int get5(int n) {
+		int tmp = n;
+		int cnt = 0;
 		
-		while(n % 2 == 0) {
-			n /= 2;
-			two--;
+		while(tmp > 0) {
+			cnt += tmp/5;
+			tmp /= 5;
 		}
 		
-		while(n % 5 == 0) {
-			n /= 5;
-			five--;
-		}
+		return cnt;
 	}
 }
