0407f528ad38b10d70df577d73d9f22020f00ff2
mslim <mslim8803@naver.com>
2019-08-13 23:34:13 +0900

    Lim

---
+++ src/lim/_Lim2146.java
@@ -0,0 +1,146 @@
+package lim;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class _Lim2146 {
+	private static int[] di = {1, 0, -1, 0};
+	private static int[] dj = {0, 1, 0, -1};
+	private static List<Integer> list = new ArrayList<Integer>();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		boolean[][] map = new boolean[n][n];
+		int[][] newMap = new int[n][n];
+		boolean[][] visit = new boolean[n][n];		
+		
+		for(int i = 0; i < n; i++) {
+			String input = br.readLine();
+			
+			for(int j = 0; j < n; j++) {
+				
+				if(input.charAt(2 * j) == '1') {
+					map[i][j] = true;
+				}
+			}
+		}
+		
+		int color = 1;
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				
+				if(!visit[i][j] && map[i][j]) {
+					colorMap(map, newMap, visit, i, j, color++, n);
+				}
+			}
+		}
+				
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {			
+				if(map[i][j]) {					
+					for(int k = 0; k < 4; k++) {
+						int nextI = i + di[k];
+						int nextJ = j + dj[k];	
+						
+						if(nextI >= n || nextI < 0 || nextJ >= n || nextJ < 0) {
+							continue;
+						}
+						
+						if(!map[nextI][nextJ]) {
+							visit = new boolean[n][n];							
+							searchBridge(map, newMap, visit, nextI, nextJ, n, newMap[i][j]);
+						}
+					}
+				}
+			}
+		}
+		
+		int min = list.get(0);
+		
+		for(int i = 1; i < list.size(); i++) {
+			
+			if(min > list.get(i)) {
+				min = list.get(i);
+			}
+		}
+		
+		System.out.println(min);
+	}
+
+	private static void searchBridge(boolean[][] map, int[][] newMap, boolean[][] visit, int i, int j, int n, int currentColor) {
+		int cnt = 1;
+		Queue<Point> queue = new LinkedList<Point>();
+		
+		visit[i][j] = true;
+		queue.add(new Point(i, j));
+		
+		outer : while(!queue.isEmpty()) {
+			int size = queue.size();
+			
+			for(int k = 0; k < size; k++) {
+				Point p = queue.poll(); 
+				
+				for(int l = 0; l < 4; l++) {
+					int nextI = p.getI() + di[l];
+					int nextJ = p.getJ() + dj[l];
+					
+					if(nextI >= n || nextI < 0 || nextJ >= n || nextJ < 0) {
+						continue;
+					}
+					
+					if(!map[nextI][nextJ]) {
+						visit[nextI][nextJ] = true;
+						queue.add(new Point(nextI, nextJ));
+					} else if(newMap[nextI][nextJ] != currentColor) {
+						list.add(cnt);
+						break outer;
+					}
+				}
+			}
+			
+			cnt++;
+		}
+	}
+	
+	static class Point {
+		private int i;
+		private int j;
+		
+		public Point(int i, int j) {
+			this.i = i;
+			this.j = j;
+		}
+
+		public int getI() {
+			return i;
+		}
+
+		public int getJ() {
+			return j;
+		}
+	}
+
+	private static void colorMap(boolean[][] map, int[][] newMap, boolean[][] visit, int i, int j, int color, int n) {
+		visit[i][j] = true;
+		newMap[i][j] = color;
+		
+		for(int k = 0; k < 4; k++) {
+			int nextI = i + di[k];
+			int nextJ = j + dj[k];
+			
+			if(nextI >= n || nextI < 0 || nextJ >= n || nextJ < 0) {
+				continue;
+			}
+			
+			if(!visit[nextI][nextJ] && map[nextI][nextJ]) {
+				colorMap(map, newMap, visit, nextI, nextJ, color, n);
+			}
+		}
+	}
+}
