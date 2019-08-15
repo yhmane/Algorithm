00c72514aa725a763109f3719f0bb40f09c1b3a2
mslim <mslim8803@naver.com>
2019-08-15 14:44:58 +0900

    Lim

---
+++ src/lim/_Lim1167_2.java
@@ -0,0 +1,60 @@
+package lim;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class _Lim1167_2 {
+	private static int diameter = 0;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int v = Integer.parseInt(br.readLine());
+		
+		@SuppressWarnings("unchecked")
+		List<Integer>[] lists = new ArrayList[v + 1];
+				
+		for(int i = 1; i <= v; i++) {
+			lists[i] = new ArrayList<Integer>();
+		}
+		
+		for(int i = 1; i <= v; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());		
+			int m;
+			
+			while((m = Integer.parseInt(st.nextToken())) != -1) {
+				lists[n].add(m);
+			}
+		}
+		
+		for(int i = 1; i <= v; i++) {
+			boolean[] visit = new boolean[v + 1];
+			dfsMethod(lists, visit, i, v, 0);
+		}
+		
+		System.out.println(diameter);
+	}
+	
+
+	private static void dfsMethod(List<Integer>[] lists, boolean[] visit, int i, int v, int distance) {
+		visit[i] = true;
+		
+		if(distance > diameter) {
+			diameter = distance;
+		}
+		
+		int realSize = lists[i].size() / 2;
+		
+		for(int j = 0; j < realSize; j++) {
+			
+			int idx = lists[i].get(2 * j);
+			
+			if(!visit[idx]) {
+				dfsMethod(lists, visit, idx, v, distance + lists[i].get(2 * j + 1));
+			}
+		}
+	}
+}
