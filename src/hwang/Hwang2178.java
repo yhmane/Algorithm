8d6c8e892bbe1c17428cc68b831773f55a9d8d52
yhmane <hwang9yh@gmail.com>
2019-08-28 09:17:13 +0900

    그래프 2178번 다시 제출

--- src/hwang/Hwang2178.java
+++ src/hwang/Hwang2178.java
@@ -1,61 +1,65 @@
 package hwang;
-import java.io.*;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Hwang2178 {
-	
-	private static int graph[][];
-	private static Queue<Node> q = new LinkedList<>();
+
 	private static int row, col;
+	private static int[][] graph;
+	private static Queue<Node> queue = new LinkedList<>();
 
 	public static class Node {
+
 		private int x;
 		private int y;
 
 		public int getX() {
 			return x;
 		}
-		
+
 		public int getY() {
 			return y;
 		}
-		
+
 		public Node(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
 	}
-
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		row = Integer.parseInt(st.nextToken());
 		col = Integer.parseInt(st.nextToken());
-
-		
 		graph = new int[row][col];
+
 		for (int i = 0; i < row; i++) {
 			String input = br.readLine();
 			for (int j = 0; j < col; j++) {
 				graph[i][j] = input.charAt(j) - '0';
 			}
 		}
-		
-		bfs();
 
+		bfs();
 	}
-	
+
 	private static void bfs() {
 
-		q.offer(new Node(0, 0));
+		queue.offer(new Node(0, 0));
 		int xxxx[] = { -1, 1, 0, 0 };
 		int yyyy[] = { 0, 0, 1, -1 };
 
-		while (!q.isEmpty()) {
-			int x = q.peek().getX();
-			int y = q.peek().getY();
-			q.poll();
+		while (!queue.isEmpty()) {
+			int x = queue.peek().getX();
+			int y = queue.peek().getY();
+			queue.poll();
 
 			for (int i = 0; i < 4; i++) {
 				int nx = x + xxxx[i];
@@ -64,7 +68,7 @@ public class Hwang2178 {
 				if (0 <= nx && nx < row && 0 <= ny && ny < col) {
 					if (graph[nx][ny] == 1) {
 						graph[nx][ny] = graph[x][y] + 1;
-						q.offer(new Node(nx, ny));
+						queue.offer(new Node(nx, ny));
 					}
 				}
 			}
