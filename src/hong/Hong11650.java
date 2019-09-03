9557fc06ee38d0f8a753ca974315db841104c122
dphong <dphong@kaoni.com>
2019-09-03 10:22:00 +0900

    prob. 11650

--- src/hong/Hong11650.java
+++ src/hong/Hong11650.java
@@ -1,7 +1,7 @@
 package hong;
 /* 
-좌표 정렬하기
-2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+좌표 정렬하기 2
+2차원 평면 위의 점 N개가 주어진다. 좌표를 y좌표가 증가하는 순으로, y좌표가 같으면 x좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
 첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
 
 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
@@ -25,12 +25,12 @@ class Point implements Comparable<Point>{
 
 	@Override
 	public int compareTo(Point o) {
-		if (this.x < o.x) {
+		if (this.y < o.y) {
 			return -1;
-		} else if (this.x == o.x) {
-			if(this.y < o.y) {
+		} else if (this.y == o.y) {
+			if(this.x < o.x) {
 				return -1;
-			} else if (this.y == o.y) {
+			} else if (this.x == o.x) {
 				return 0;
 			} else {
 				return 1;
