b6908308c3e2122052f32391eed86bca3559eb0a
Hong Daepyo <h1o1n1g1@nate.com>
2019-09-05 23:51:12 +0900

    prob. 10989

---
+++ src/hong/Hong10989.java
@@ -0,0 +1,38 @@
+package hong;
+/*
+ * 수 정렬하기 3
+ * N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+ * 첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다. 이 수는 10,000보다 작거나 같은 자연수이다.
+ * 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+ * 
+ * */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Hong10989 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < N; i++) {
+			bw.append(arr[i] + "\n");
+		}
+		
+		bw.flush();
+		br.close();
+	}
+
+}
