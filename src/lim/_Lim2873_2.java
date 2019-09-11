558ce09f2fb6c6037f5afd14c1be61b17c5663b7
mslim8803 <mslim8803@kaoni.com>
2019-09-11 13:00:13 +0900

    Lim

--- src/lim/_Lim2873_2.java
+++ src/lim/_Lim2873_2.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _Lim2873_2 {
+	private static int minI = 0;
+	private static int minJ = 1;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,11 +22,30 @@ public class _Lim2873_2 {
 		} else if(c % 2 == 1) {
 			skipReadLine(br, r);
 			answer = makeSimpleAnswer(c, r, 'D', 'U', 'R');
-		} else {
-			
+		} else {		
+			getMinScoreSpot(br, r, c);
 		}
 		
 		System.out.println(answer);
+	}
+
+	private static void getMinScoreSpot(BufferedReader br, int r, int c) throws IOException {
+		StringTokenizer st;
+		int min = Integer.MAX_VALUE;
+		
+		for(int i = 0; i < r; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			for(int j = 0; j < c; j++) {
+				int score = Integer.parseInt(st.nextToken());
+				
+				if((i + j) % 2 == 1 && min > score) {
+					min = score;
+					minI = i;
+					minJ = j;
+				}
+			}
+		}
 	}
 
 	private static void skipReadLine(BufferedReader br, int r) throws IOException {
