d20208c7917e9979a33ce9febcda815fea26ab36
mslim8803 <mslim8803@kaoni.com>
2019-09-18 13:12:37 +0900

    Lim

---
+++ src/lim/_Lim1697.java
@@ -0,0 +1,14 @@
+package lim;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _Lim1697 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());		
+	}
+}
