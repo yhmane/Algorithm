b09274c23d84d7397d06e95fa1680894467c2f56
mslim <mslim8803@naver.com>
2019-11-12 08:54:16 +0900

    Lim

--- src/lim/_Lim2011.java
+++ src/lim/_Lim2011.java
@@ -2,6 +2,7 @@ package lim;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class _Lim2011 {	
 	public static void main(String[] args) throws Exception {
@@ -49,7 +50,8 @@ public class _Lim2011 {
 				}
 			}
 		}
-		
+		// 7220 -> 1
+		System.out.println(Arrays.toString(ans));
 		System.out.println(ans[n - 1] % 1000000);
 	}
 }
