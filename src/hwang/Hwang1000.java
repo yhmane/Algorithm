72a57e6f32dd9bbed8d177f0bde1e4e2fc300291
yhmane <hwang9yh@gmail.com>
2020-10-25 23:10:29 +0900

    알고리즘 입출력

---
+++ src/hwang/Hwang1000.java
@@ -0,0 +1,13 @@
+package hwang;
+
+import java.util.Scanner;
+
+public class Hwang1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arr = scanner.nextLine().split(" ");
+
+        System.out.println(Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]));
+    }
+}
+
---
+++ src/hwang/Hwang2558.java
@@ -0,0 +1,13 @@
+package hwang;
+
+import java.util.Scanner;
+
+public class Hwang2558 {
+    public static void main(String[] arhs) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        System.out.println(num1 + num2);
+    }
+}
