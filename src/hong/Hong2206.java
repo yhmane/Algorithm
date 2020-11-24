4ae6f2c5754d98537f3543ca17f2518f65f50a3c
dphong <dphong@kaoni.com>
2020-11-24 17:58:10 +0900

    prob. 2206

--- src/hong/Hong2206.java
+++ src/hong/Hong2206.java
@@ -11,13 +11,12 @@ import java.util.StringTokenizer;
 
 public class Hong2206 {
 	static int[][] map;
-	static int[][] resultMap;
-	static boolean[][] visited;
+	static boolean[][][] visited;
 	static int N;
 	static int M;
 	static int[] dx = {1, -1, 0, 0};
 	static int[] dy = {0, 0, 1, -1};
-	static int min = Integer.MAX_VALUE;
+	static int min = 0;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,7 +26,7 @@ public class Hong2206 {
 		M = Integer.parseInt(st.nextToken());
 		
 		map = new int[N][M];
-		visited = new boolean[N][M];
+		visited = new boolean[N][M][2];  // idx: 0 은 부순 벽 방문, 1은 일반 벽 방문;
 		
 		for (int i = 0; i < N ; i++) {
 			String strRow = br.readLine();
@@ -41,55 +40,60 @@ public class Hong2206 {
 			return;
 		}
 		
-		for (int i = 0; i < N ; i++) {
-			for (int j = 0; j < M; j++) {
-				if (map[i][j] == 1) {
-					resultMap = new int[N][M];
-					visited = new boolean[N][M];
-					map[i][j] = 0;
-					bfs();
-					map[i][j] = 1;
-				}
-			}
-		}
-		
-		if (min == Integer.MAX_VALUE) {
-			System.out.println(-1);
-		} else {
-			System.out.println(min + 1);
-		}
+		bfs();
 	}
 	
 	private static void bfs() {
-		Queue<Integer> qRow = new LinkedList<Integer>();
-		Queue<Integer> qCol = new LinkedList<Integer>();
+		Queue<Point> pq = new LinkedList<Point>();
 		
-		qRow.offer(0);
-		qCol.offer(0);
-		visited[0][0] = true;
+		pq.offer(new Point(0, 0, 1, 0));
+		visited[0][0][0] = true;
+		visited[0][0][1] = true;
 		
-		while (!qRow.isEmpty()) {
-			int tempRow = qRow.poll();
-			int tempCol = qCol.poll();
+		while (!pq.isEmpty()) {
+			Point p = pq.poll();
+			
+			if (p.row == N - 1 && p.col == M -1) {
+//				map[tempRow][tempCol][0] = map[tempRow][tempCol][0];
+				System.out.println(p.dist);
+				return;
+			}
 			
 			for (int i = 0; i < 4; i++) {
-				int newRow = tempRow + dx[i];
-				int newCol = tempCol + dy[i];
+				int newRow = p.row + dx[i];
+				int newCol = p.col + dy[i];
+				int breakWall = p.breakWall;
+				int dist = p.dist;
 				
 				if (newRow >= 0 && newCol >= 0 && newRow < N && newCol < M) {
-					if (map[newRow][newCol] == 0 && !visited[newRow][newCol]) {
-						visited[newRow][newCol] = true;
-						qRow.offer(newRow);
-						qCol.offer(newCol);
-						resultMap[newRow][newCol] = resultMap[tempRow][tempCol] + 1;
+					if (map[newRow][newCol] == 0) { // 벽이 아닌 곳
+						if (!visited[newRow][newCol][breakWall]) {
+							visited[newRow][newCol][breakWall] = true;
+							pq.offer(new Point(newRow, newCol, dist + 1, breakWall));
+						}
+					} else { // 벽인 곳
+						if (breakWall == 0 && !visited[newRow][newCol][1]) {
+							visited[newRow][newCol][1] = true;
+							pq.offer(new Point(newRow, newCol,  dist + 1, 1));
+						}
 					}
 				}
 			}
 		}
+		System.out.println(-1);
+	}
+	
+	private static class Point {
+		int row;
+		int col;
+		int dist;
+		int breakWall; // 0 = 벽을 부순적 없음, 1 = 벽을 부순적 있음
 		
-		int result = resultMap[N - 1][M - 1];
-		if (result > 0) {
-			min = Math.min(min, result);
+		Point (int row, int col, int dist, int breakWall) {
+			this.row = row;
+			this.col = col;
+			this.dist = dist;
+			this.breakWall = breakWall;
 		}
 	}
 }
