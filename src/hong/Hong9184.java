8a5c3d625b3d30d4fcb20fe12ffa98f167c519d1
Hong Daepyo <h1o1n1g1@nate.com>
2021-02-01 17:24:15 +0900

    prob. 9184

--- src/hong/Hong9184.java
+++ src/hong/Hong9184.java
@@ -8,9 +8,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Hong9184 {
+	static int[][][] arr = new int[21][21][21];
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		getAnswer();
+
 		StringTokenizer st;
 		
 		StringBuilder sb = new StringBuilder();
@@ -25,12 +29,34 @@ public class Hong9184 {
 			}
 			
 			sb.append("w(" + a + ", " + b + ", " + c + ") = ");
-			sb.append(getAnswer()).append("\n");
+			if (a <= 0 || b <= 0 || c <= 0) {
+				sb.append(1).append("\n");
+			} else if (a > 20 || b > 20 || c > 20) {
+				sb.append(arr[20][20][20]).append("\n");
+			} else {
+				sb.append(arr[a][b][c]).append("\n");
+			}
 		}
+		
+		System.out.println(sb);
 		
 	}
 	
-	public static String getAnswer() {
-		return "";
+	public static void getAnswer() {
+		for (int i = 0; i <= 20; i++) {
+			for (int j = 0; j <= 20; j++) {
+				for (int k = 0; k <= 20; k++) {
+					if (i < 1 || j < 1 || k < 1) {
+						arr[i][j][k] = 1;
+					} else {
+						if (i < j && j < k) {
+							arr[i][j][k] = arr[i][j][k - 1] + arr[i][j - 1][k - 1] - arr[i][j - 1][k];
+						} else {
+							arr[i][j][k] = arr[i - 1][j][k] + arr[i - 1][j - 1][k] + arr[i - 1][j][k - 1] - arr[i - 1][j - 1][k - 1];
+						}
+					}
+				}
+			}
+		}
 	}
 }
